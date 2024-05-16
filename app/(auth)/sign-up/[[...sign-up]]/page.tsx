f2b109e7d540a85c95817c15571b58877b181592
Divnoor <divnoorsingh4602@gmail.com>
2024-05-16 14:38:30 +0530

    changed middlewear for clerk auth and webhooks

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp path="/sign-up" />;
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-auth-light bg-cover bg-center bg-no-repeat dark:bg-auth-dark">
+        <SignUp path="/sign-up" />
+      </div>
+    </>
+  );
 }
