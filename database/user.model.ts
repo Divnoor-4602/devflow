fa8585fbec62ae47a599b3ccd373b7b8eb861f05
Divnoor <divnoorsingh4602@gmail.com>
2024-05-16 16:01:17 +0530

    changed middlewear for clerk auth and webhooks

--- database/user.model.ts
+++ database/user.model.ts
@@ -3,7 +3,7 @@ import { Schema, models, model, Document } from "mongoose";
 export interface IUser extends Document {
   clerkId: string;
   name: string;
-  username: string;
+  username?: string;
   email: string;
   password?: string;
   bio?: string;
@@ -19,7 +19,7 @@ export interface IUser extends Document {
 const userSchema = new Schema({
   clerkId: { type: String, required: true },
   name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String },
   bio: { type: String },
